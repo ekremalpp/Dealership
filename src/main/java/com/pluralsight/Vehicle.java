dc00f60274280ed584e21333bea3207a7e7dfaaa
Ekrem <ealp@pgh.yearup.org>
2024-10-28 19:11:30 -0400

    added To String method on Vehicle class

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -84,4 +84,10 @@ public class Vehicle {
     public void setPrice(double price) {
         this.price = price;
     }
+    @Override
+    public String toString() {
+        return
+                vin + "|" + year + "|" + odometer + "|" + make + "|" + model + "|" + vehicleType + "|" + color + "|" + price;
+
+    }
 }
