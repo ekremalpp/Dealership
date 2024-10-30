f51d4e39eddecbb01f28632bc6dad2b5e6b66f7c
Ekrem <ealp@pgh.yearup.org>
2024-10-30 15:14:31 -0400

    uploaded Program class

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -1,5 +1,19 @@
 package com.pluralsight;
 
 public class Program {
+    public static void main(String[] args) {
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
+        // Application is starting
+        System.out.println("Welcome to the Dealership Management System!");
 
+        // Create a user interface object
+        UserInterface userInterface = new UserInterface();
+
+        // Display the user menu
+        userInterface.display();
+
+        // Application exit message
+        System.out.println("Thank you for using the Dealership Management System. Goodbye!");
+    }
 }
+
